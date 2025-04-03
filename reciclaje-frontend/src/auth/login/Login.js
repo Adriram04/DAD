8d18f90a8ce0abc5525318eaac4a432ca3b11b68
Amadeusz <114829302+thecodingslave123@users.noreply.github.com>
2025-04-03 14:43:46 +0200

    Update Login.js

--- reciclaje-frontend/src/auth/login/Login.js
+++ reciclaje-frontend/src/auth/login/Login.js
@@ -18,6 +18,7 @@ export default function Login() {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
+      mode: 'cors'
     });
 
     if (res.ok) {
