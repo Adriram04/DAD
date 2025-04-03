7086365ce64e1254097f70b96229182361bcaee8
Amadeusz <114829302+thecodingslave123@users.noreply.github.com>
2025-04-04 00:39:59 +0200

    Update Launcher.java

--- reciclaje-backend/src/main/java/com/hackforchange/reciclaje_backend/Launcher.java
+++ reciclaje-backend/src/main/java/com/hackforchange/reciclaje_backend/Launcher.java
@@ -1,13 +1,14 @@
 package com.hackforchange.reciclaje_backend;
 
-
 import com.hackforchange.reciclaje_backend.config.*;
 import io.vertx.core.Vertx;
 import io.vertx.core.DeploymentOptions;
 
 public class Launcher {
-
     public static void main(String[] args) {
+        // Deshabilitar el DnsResolver para evitar errores en Azure
+        System.setProperty("vertx.disableDnsResolver", "true");
+
         Vertx vertx = Vertx.vertx();
 
         ConfigLoader.load(vertx).onSuccess(config -> {
@@ -18,11 +19,10 @@ public class Launcher {
             vertx.deployVerticle(new MainApp(), options)
                 .onSuccess(id -> System.out.println("🟢 MainApp desplegado con ID: " + id))
                 .onFailure(err -> System.err.println("❌ Fallo al desplegar MainApp: " + err.getMessage()));
-            
+
             System.out.println("🟡 Después de intentar desplegar MainApp...");
         }).onFailure(err -> {
             System.err.println("❌ Error al cargar configuración: " + err.getMessage());
         });
-
     }
 }
